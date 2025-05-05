44685e3c839ea1d0abce34d0b6b406721701bdcd
Greg Delgado <grd888@gmail.com>
2025-05-06 06:22:54 +0800

    install Clerk auth framework

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,11 @@
-import { Image, Pressable, Text, TouchableOpacity, View } from "react-native";
-import { styles } from "../../styles/auth.styles";
 import { Link } from "expo-router";
+import { View } from "react-native";
+import { styles } from "../../styles/auth.styles";
 
 export default function Index() {
   return (
     <View style={styles.containers}>
-
       <Link href="/notifications">Visit notifications screen </Link>
-      
-
     </View>
   );
 }
