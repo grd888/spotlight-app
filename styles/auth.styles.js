f6dae1a2cf8d47abf2bf5a409c04f76d67f6cd9e
Greg Delgado <grd888@gmail.com>
2025-05-05 21:15:27 +0800

    delete chirp

---
+++ styles/auth.styles.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  containers: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "red",
+  },
+  image: {
+    width: 200,
+    height: 200,
+  },
+});
